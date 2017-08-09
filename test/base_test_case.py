6a2d36000af948a5e7fb24b978390940c9476ffe
Tony Foerster <tony@phdata.io>
2017-08-09 10:53:39 -0500

    Add logging to base test

--- test/base_test_case.py
+++ test/base_test_case.py
@@ -3,6 +3,7 @@ import os
 import shlex
 import subprocess
 import time
+import logging
 
 import pytest
 import requests
@@ -15,6 +16,7 @@ def sdc(request):
     popen_kwargs = {}
     proc = subprocess.Popen(["docker-compose", "-f", compose_file, "up"], **popen_kwargs)
     def cleanup():
+        logging.info("cleaning up process")
         proc.kill()
         subprocess.call(
             ["docker-compose", "-f", compose_file, "kill"],
@@ -22,6 +24,7 @@ def sdc(request):
         subprocess.call(
             ["docker-compose", "-f", compose_file, "rm", "-f"],
             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        logging.info("finished cleanup")
     request.addfinalizer(cleanup)
     curl_until_success(18630)
     return proc
@@ -30,15 +33,19 @@ def get_docker_host():
     docker_host = os.environ.get('DOCKER_HOST', '').strip()
     if not docker_host:
         return '127.0.0.1'
+    logging.info("using docker host: " + docker_host)
     return docker_host
 
 def curl_until_success(port, endpoint="/", params={}):
     timeout = datetime.datetime.now() + datetime.timedelta(seconds=TIMEOUT_SECONDS)
+    url = "http://{}:{}{}".format(
+                get_docker_host(), port, endpoint)
     while datetime.datetime.now() < timeout:
         try:
-            response = requests.get("http://{}:{}{}".format(
-                get_docker_host(), port, endpoint), params=params)
+            response = requests.get(url, params=params)
+            logging.info("received response from endpoint")
         except requests.exceptions.ConnectionError:
+            logging.info("waiting for service")
             pass
         else:
             return response
@@ -46,4 +53,5 @@ def curl_until_success(port, endpoint="/", params={}):
     raise Exception("service didn't start in time")
 
 def execute_cmd(cmd):
+    logging.info("executing command: " + cmd)
     assert subprocess.check_call(shlex.split(cmd)) == 0
